da6193687a708d8368ed1d3be29766aa634d360d
Gwydion <68437867+Gwyd0@users.noreply.github.com>
2023-09-24 16:49:00 +0100

    Add files via upload

--- content.js
+++ content.js
@@ -2,7 +2,9 @@ let logs = [];
 let bwc;
 let answer;
 let logString;
+let answerString;
 let i; 
+
 
 let SettingsAutoLogin = true;
 let SettingsBookworkChecks = true;
@@ -55,32 +57,57 @@ function autoLogin() {
 	}
 }
 
-//function debug() {
-	//const tiles = document.getElementsByClassName("_CardSlot_1yby8_645");
-	//const test = document.getElementsByClassName("katex-mathml");
-	//console.log("");
-	//for (x = 0; x < test.length; x++) {
-		//console.log(test[x].innerText);
-	//}
-	//console.log(test[0]);
-	//console.log(tiles[0].children[0]);
-//}
+
+function test() {
+	let answerString="";
+	const submit = document.querySelector("button[class^='_ButtonBase_10evl_1 _FocusTarget_1nxry_1 _ButtonMd_10evl_27 _ButtonBlue_10evl_51 _ButtonContained_10evl_81']");
+	if(document.querySelector('div[class^="_Chip_1l3e3_1 _Selected_1l3e3_13"]') && !submit.classList.contains("_Disabled_10evl_89")&&submit.innerText == "Submit answer"){
+	
+		let bwc=document.querySelector('div[class^="_Chip_1l3e3_1 _Selected_1l3e3_13"]').innerText.replace("Bookwork code: ", "");
+		if (document.getElementsByClassName('katex-mathml') != null) {
+			tiles = document.getElementsByClassName('katex-mathml');
+			submit.addEventListener('click', function() {
+				for (x = 0; x < tiles.length; x++) {
+					answerString = answerString + " " + tiles[x].innerText;
+				}
+				log(bwc, answerString);
+			});
+		}
+		if(document.querySelector("input[placeholder='Enter number']")){
+			const inputs = document.querySelectorAll("input[placeholder='Enter number']");
+			submit.addEventListener('click', function() {
+				for (x = 0; x < inputs.length; x++) {
+					answerString = answerString + " " + inputs[x].value;
+				}
+				log(bwc, answerString);
+			});
+		}
+		if(document.querySelector("._OptionSelected_1yby8_511")){
+			const inputs = document.querySelectorAll("._OptionSelected_1yby8_511");
+			submit.addEventListener('click', function() {
+				for (x = 0; x < inputs.length; x++) {
+					answerString = answerString + " " + inputs[x].innerText;
+				}
+				log(bwc, answerString);
+			});
+		}
+	}
+	
+}
 
 function log(bwc, answer) {
-	if (answer == "-") {
+	if (answer == "") {
 		return;
 	}
 	const submit = document.querySelector("button[class^='_ButtonBase_10evl_1 _FocusTarget_1nxry_1 _ButtonMd_10evl_27 _ButtonBlue_10evl_51 _ButtonContained_10evl_81']");
-	if (answer != "" && submit != null) {
-		submit.addEventListener('click', function() {
-			const logString = "[BWC] "+bwc+" [ANSWER] "+answer;
-			if (logString!=logs[logs.length-1] && logString!=logs[logs.length-2] && logString!=logs[logs.length-3]) {
-				console.log(logString);
-				logs.push(logString);
-			}}); 	
+	const logString = "[BWC] "+bwc+" [ANSWER] "+answer;
+	if (logString!=logs[logs.length-1] && logString!=logs[logs.length-2] && logString!=logs[logs.length-3]) {
+		console.log(logString);
+		logs.push(logString);	
 	}
 }
 
+/*
 function logAnswer() {
 	if(document.querySelector('div[class^="_Chip_1l3e3_1 _Selected_1l3e3_13"]')){
 		let bwc=document.querySelector('div[class^="_Chip_1l3e3_1 _Selected_1l3e3_13"]').innerText.replace("Bookwork code: ", "");
@@ -89,10 +116,10 @@ function logAnswer() {
 			console.log("[BOOKWORK CHECK] " + bwc);
 			bwc=bwc.replace("Bookwork ", "");
 			const wacOptions=document.getElementsByClassName("answer-part");
-			for (x = 0; x < allLogs.length; x++) {
-				if (allLogs[x].includes(bwc)) {
+			for (x = 0; x < logs.length; x++) {
+				if (logs[x].includes(bwc)) {
 					for (i = 0; i < wacOptions.length; i++) {
-						if (allLogs[x].replace("[BWC] "+bwc+" [ANSWER] ", "").includes(wacOptions[i].innerText)) {
+						if (logs[x].replace("[BWC] "+bwc+" [ANSWER] ", "").includes(wacOptions[i].innerText)) {
 							console.log(wacOptions[i].innerText);
 						}
 					}	
@@ -129,11 +156,12 @@ function logAnswer() {
 		}
 	}
 }
+*/
 // START Sparx Maths Student Login
 
 console.log("Loaded SparxBwk");
 autoLogin();
 let elementCheckingInterval = setInterval(function(){
-	logAnswer();
+	test();
 }, 400);
 
